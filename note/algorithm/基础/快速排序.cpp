a4f55a204af796610f30ccad1fc1f70dc4a01e04
root <1984737645@qq.com>
2020-03-30 19:53:48 +0800

    03-30-算法笔记维护

---
+++ note/algorithm/基础/快速排序.cpp
@@ -0,0 +1,62 @@
+/*************************************************************************
+ > FileName:
+ > Author:      Lance
+ > Mail:        lancelot_hcs@qq.com
+ > Date:        9102.1.8
+ > Description:
+ ************************************************************************/
+//#include <bits/stdc++.h>
+#pragma comment(linker, "/STACK:102400000,102400000")//add_stack
+#include <algorithm>
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <vector>
+#include <cstdio>
+#include <bitset>
+#include <string>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+using namespace std;
+typedef long long ll;
+const double pi = acos(-1.0);
+const int eps = 1e-10;
+const int mod = 1e9 + 7;
+#define debug(a) cout << "*" << a << "*" << endl
+const int INF = 0x3f3f3f3f;//int2147483647//ll9e18//unsigned ll 1e19
+const int maxn = 100005;
+
+int a[maxn];
+int bsearch_1(int l, int r, int x)
+{
+    while (l < r)
+    {
+        int mid = l + r >> 1;
+        if (a[mid] >= x) r = mid;
+        else l = mid + 1;
+    }
+    return l;
+}
+
+void solve()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> a[i];
+    }
+    cout << bsearch_1(0, 9, 3);
+}
+
+int main()
+{
+    solve();
+//    freopen("F:/Overflow/in.txt","r",stdin);
+//    ios::sync_with_stdio(false);
+    return 0;
+}
+
